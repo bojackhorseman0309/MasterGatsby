c5cd0cc1481131a8c46e194c28a4d9288713bc99
Alonso Araya Calvo <alonso@elumen.info>
2020-12-31 12:50:56 -0600

    Gatsby/ Module 6

--- starter-files/gatsby/src/pages/pizzas.js
+++ starter-files/gatsby/src/pages/pizzas.js
@@ -1,11 +1,13 @@
 import { graphql } from 'gatsby';
 import React from 'react';
 import PizzaList from '../components/PizzaList';
+import ToppingsFilter from '../components/ToppingsFilter';
 
 export default function PizzasPage({ data }) {
   const pizzas = data.pizzas.nodes;
   return (
     <>
+      <ToppingsFilter />
       <PizzaList pizzas={pizzas} />
     </>
   );
