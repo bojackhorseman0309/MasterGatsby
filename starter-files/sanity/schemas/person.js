0a6917f51e83215a8206f6e0be2e5488c11950f3
Alonso Araya Calvo <alonso@elumen.info>
2020-12-31 09:40:29 -0600

    Sanity/ Module 4

---
+++ starter-files/sanity/schemas/person.js
@@ -0,0 +1,38 @@
+import { MdPerson as icon } from 'react-icons/md';
+
+export default {
+  name: 'person',
+  title: 'Slicemasters',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      description: 'Tell us a bit about this slicemaster',
+    },
+    {
+      name: 'image',
+      title: 'image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
